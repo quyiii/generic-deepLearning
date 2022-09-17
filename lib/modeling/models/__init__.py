bd4796b281b47279c94e2fc7ec17ff318c17f6d3
quyiii <18328485788@163.com>
2022-09-17 15:12:48 +0800

    add weight init and to device init

--- lib/modeling/models/__init__.py
+++ lib/modeling/models/__init__.py
@@ -6,6 +6,7 @@ import importlib
 import functools
 import torch
 import torch.nn as nn 
+from torch.nn import init
 
 def get_model_class(cfg):
     file_name = 'lib.modeling.models.'+cfg.INPUT.TYPE.lower()+'_models'
@@ -57,9 +58,39 @@ def init_net(net, init_type='normal', init_gain=0.02, device='cuda', gpu_ids=[0]
         if len(gpu_ids) == 0:
             raise RuntimeError('no gpu_ids for run program')
         net = nn.DataParallel(net, gpu_ids)
-        net.to(gpu_ids[0])
+        net.to("cuda:" + gpu_ids[0])
     elif device.lower() == 'cpu':
         net.to('cpu')
     else: 
         raise NotImplementedError('no device {}'.format(device))
+    init_weight(net, init_type, init_gain)
     return net
+
+def init_weights(net, init_type='normal', init_gain=0.02):
+    """Initialize network weights
+
+    Parameters:
+        net (network)   -- network to be initialized
+        init_type (str) -- the name of an initialization method: normal | xavier | kaiming | orthogonal
+        init_gain (float)    -- scaling factor for normal, xavier and orthogonal.
+    """
+    def init_func(m):
+        classname = m.__class__.__name__
+        if hasattr(m, 'weight') and (classname.find('Conv') != -1 or classname.find('Linear') != -1):
+            if init_type == 'normal':
+                init.normal_(m.weight.data, 0.0, init_gain)
+            elif init_type == 'xavier':
+                init.xavier_normal_(m.weight.data, gain=init_gain)
+            elif init_type == 'kaiming':
+                init.kaiming_normal_(m.weight.data, a=0, mode='fan_in')
+            elif init_type == 'orthogonal':
+                init.orthogonal_(m.weight.data, gain=init_gain)
+            else:
+                raise NotImplementedError('initialization mothod {} is not implemented'.format(norm_type))
+            if hasattr(m, 'bias') and m.bias is not None:
+                init.constant_(m.bias.data, 0.0)
+        elif classname.find('BatchNorm2d') != -1:
+            init.normal_(m.weight.data, 1.0, init_gain)
+            init.constant_(m.bias.data, 0.0)
+    # 模型的所有模块都要初始化
+    net.apply(init_func)
